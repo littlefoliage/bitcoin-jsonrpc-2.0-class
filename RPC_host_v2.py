aeb6236248efa42eb9c731ca704a71c712459f13
littlefoliage <littlefoliage@hotmail.com>
2020-11-12 17:24:34 -0500

    Create RPC_host_v2.py

---
+++ RPC_host_v2.py
@@ -0,0 +1,39 @@
+import requests,json
+class RPCHost(object):
+    def __init__(self, url):
+        self._session = requests.Session()
+        self._url = url
+        self._headers = {'content-type': 'application/json'}
+    def call(self, rpcMethod, *params):
+        payload=None
+        if not isinstance(rpcMethod, list)  :
+            payload = json.dumps({"method": rpcMethod, "params": list(params), "jsonrpc": "2.0"})
+        else:
+            request_list=[]
+            
+            for i in range(0,len(rpcMethod)):
+                request_list.append({"method": rpcMethod[i], "params": list(params[0][i]), "jsonrpc": "2.0"})
+            payload = json.dumps(request_list)
+            
+        tries = 5
+        hadConnectionFailures = False
+        while True:
+            try:
+                response = self._session.post(self._url, headers=self._headers, data=payload)
+            except requests.exceptions.ConnectionError:
+                tries -= 1
+                if tries == 0:
+                    raise Exception('Failed to connect for remote procedure call.')
+                hadFailedConnections = True
+                print("Couldn't connect for remote procedure call, will sleep for five seconds and then try again ({} more tries)".format(tries))
+                time.sleep(10)
+            else:
+                if hadConnectionFailures:
+                    print('Connected for remote procedure call after retry.')
+                break
+        if not response.status_code in (200, 500):
+            raise Exception('RPC connection failure: ' + str(response.status_code) + ' ' + response.reason)
+        responseJSON = response.json()
+        if 'error' in responseJSON and responseJSON['error'] != None:
+            raise Exception('Error in RPC call: ' + str(responseJSON['error']))
+        return responseJSON 
